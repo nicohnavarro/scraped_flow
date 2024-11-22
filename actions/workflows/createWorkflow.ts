ab74c33959a7277513b050bd5e2c713716671598
Nicolás Navarro <nicohnavarro@gmail.com>
2024-11-23 00:00:48 +0100

    feature: prisma validate

--- actions/workflows/createWorkflow.ts
+++ actions/workflows/createWorkflow.ts
@@ -1,5 +1,26 @@
 "use server";
 
-import { createWorkflowSchemaType } from "@/schema/workflow";
+import prisma from "@/lib/prisma";
+import {
+  createWorkflowSchema,
+  createWorkflowSchemaType,
+} from "@/schema/workflow";
+import { auth } from "@clerk/nextjs/server";
 
-export async function CreateWorkflow(form: createWorkflowSchemaType) {}
+export async function CreateWorkflow(form: createWorkflowSchemaType) {
+  const { success, data } = createWorkflowSchema.safeParse(form);
+  if (!success) throw new Error("invalid form data");
+
+  const { userId } = auth();
+
+  if (!userId) throw new Error("unathenticated");
+
+  const result = await prisma.workflow.create({
+    data: {
+      userId,
+      status: "DRAFT",
+      definition: "TODO",
+      ...data,
+    },
+  });
+}
