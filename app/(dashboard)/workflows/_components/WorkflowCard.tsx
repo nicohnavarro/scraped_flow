64fd832617e8141d6876e3c2b416b576388f9dba
Nicolás Navarro <nicohnavarro@gmail.com>
2024-12-01 22:14:43 +0100

    feat: add workflow actions

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -4,8 +4,25 @@ import React from "react";
 import { Workflow } from "@prisma/client";
 import { Card, CardContent } from "@/components/ui/card";
 import { WorkflowStatus } from "@/types/workflow";
-import { FileTextIcon, PlayIcon } from "lucide-react";
+import {
+  FileTextIcon,
+  MoreVerticalIcon,
+  PlayIcon,
+  ShuffleIcon,
+  TrashIcon,
+} from "lucide-react";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import TooltipWrapper from "@/components/TooltipWrapper";
 
 const statusColors = {
   [WorkflowStatus.DRAFT]: "bg-yellow-400 text-yellow-600",
@@ -17,20 +34,77 @@ function WorkflowCard({ workflow }: { workflow: Workflow }) {
   return (
     <Card className="border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark:shadow-primar">
       <CardContent className=" p-4 flex items-center justify-between h-[100px]">
-        <div
-          className={cn(
-            "w-10 h-10 rounded-full flex items-center justify-center",
-            statusColors[workflow.status as WorkflowStatus]
-          )}
-        >
-          {isDraft ? (
-            <FileTextIcon className="h-5 w-5" />
-          ) : (
-            <PlayIcon className="h-5 w-5 text-white" />
-          )}
+        <div className="flex items-center justify-end space-x-3">
+          <div
+            className={cn(
+              "w-10 h-10 rounded-full flex items-center justify-center",
+              statusColors[workflow.status as WorkflowStatus]
+            )}
+          >
+            {isDraft ? (
+              <FileTextIcon className="h-5 w-5" />
+            ) : (
+              <PlayIcon className="h-5 w-5 text-white" />
+            )}
+          </div>
+          <div className="">
+            <h3 className="text-base font-bold text-muted-foreground flex items-center">
+              <Link
+                href={`/workflow/editor/${workflow.id}`}
+                className="flex items-center hover:underline"
+              >
+                {workflow.name}
+              </Link>
+              {isDraft && (
+                <span className="ml-2 px-2 py-0.5 text-xs font-medium bg-yellow-100 text-yellow-800 rounded-full">
+                  Draft
+                </span>
+              )}
+            </h3>
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Link
+            href={`/workflow/editor/${workflow.id}`}
+            className={cn(
+              buttonVariants({
+                variant: "outline",
+                size: "sm",
+              }),
+              "flex items-center gap-2"
+            )}
+          >
+            <ShuffleIcon size={16} />
+            Edit
+          </Link>
+          <WorkflowActions />
         </div>
       </CardContent>
     </Card>
+  );
+}
+
+function WorkflowActions() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"outline"} size={"sm"}>
+          <TooltipWrapper content={"More actions"}>
+            <div className="flex items-center justify-center w-full h-full">
+              <MoreVerticalIcon size={18} />
+            </div>
+          </TooltipWrapper>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="text-destructive flex items-center gap-2">
+          <TrashIcon size={16} />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
 
