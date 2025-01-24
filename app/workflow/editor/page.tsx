f082a288dd18bf649bf1a0fd1febc761bf0eeb3c
Nicolás Navarro <nicohnavarro@gmail.com>
2025-01-24 19:12:23 +0100

    prisma workflow

--- app/workflow/editor/page.tsx
+++ app/workflow/editor/page.tsx
@@ -1,12 +1,19 @@
+import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
-function page({ params }: { params: { workflowId: string } }) {
+async function page({ params }: { params: { workflowId: string } }) {
   const { workflowId } = params;
   const { userId } = auth();
 
   if (!userId) return <div>unauthenticated</div>;
 
+  const workflow = await prisma.workflow.findUnique({
+    where: {
+      id: workflowId,
+      userId,
+    },
+  });
   return <div>page</div>;
 }
 
